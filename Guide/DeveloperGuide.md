91273ac653af01854cb2e1104218016cedde26f6
Antoine Aubert <antoine.aubert@edu.esiee.fr>
2023-10-12 15:06:44 +0200

    update de msh

--- Guide/DeveloperGuide.md
+++ Guide/DeveloperGuide.md
@@ -5,12 +5,37 @@ Faire un df avec idh par tranche + continent pour comprendre les outliers et les
 
 ### To do : 
 1. typage des données 
-   2. les transformé en numpy ? 
+   1. les transformé en numpy ? 
 2. ajouter les continents en mode hot-one encoding
 3. ajouter l'IDH
-2. mise a l'echelle ? 
-3. identifier les types de données manquantes
-4. identifier les outliers 
+4. mise a l'échelle ? 
+5. identifier les types de données manquantes
+6. identifier les outliers 
 
 --- 
 
+## Project structure : 
+```shell
+- assets/
+  |-- style.css
+- dataset
+- Guide
+- pages
+   |-- analytics.py
+   |-- home.py
+   |-- archive.py
+  - components
+    |-- footer.py
+    |-- ....py
+- app.py
+- .gitignore
+- LICENSE
+- process_data.ipynb
+- README.md
+- tools.py
+```
+
+## Multi-pages :
+The multi-pages property is set just like in the [official documentation](https://dash.plotly.com/urls).
+
+The asset system is detailed [here](https://dash.plotly.com/external-resources).
--- pages/useGuide.py
+++ pages/useGuide.py
@@ -1,12 +1,23 @@
 import dash
-from dash import html, dcc, callback, Input, Output
+from dash import html, dcc
 
 dash.register_page(__name__, order=3)
 
+
+markdown_text = '''
+### Dash and Markdown
+
+Dash apps can be written in Markdown.
+Dash uses the [CommonMark](http://commonmark.org/)
+specification of Markdown.
+Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
+if this is your first introduction to Markdown!
+'''
+
 layout = html.Div([
     html.H1('This is our user guide page'),
+    dcc.Markdown(children=markdown_text),
     html.Div('...'),
     html.P('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quam est, ultrices egestas sapien a, interdum tempus arcu. Sed sed porta turpis. Praesent malesuada, augue vulputate commodo commodo, felis dui fermentum odio, a maximus neque urna eget purus. Donec tempus auctor molestie. Sed varius mattis magna et iaculis. Donec pharetra orci vitae urna rutrum, non vulputate felis eleifend. Sed tincidunt volutpat elementum. Morbi fermentum vehicula sem ut volutpat. Phasellus blandit tortor ante, eleifend vulputate libero accumsan sed. Aenean a mattis magna. Nam vehicula non odio sed vulputate. Nullam malesuada neque vulputate, tristique quam non, fermentum nunc. Praesent feugiat, metus non congue semper, ante lectus feugiat orci, a interdum lacus ligula eget lectus. Sed quis tellus egestas, tristique leo et, interdum sapien. Curabitur rutrum mi vitae ligula pulvinar fringilla. Aenean cursus volutpat sem ut elementum. Morbi sed arcu in lorem ullamcorper volutpat a vel orci. Donec in tincidunt leo. Integer tempus efficitur nisi vel porttitor. Donec vel fringilla quam, ac sollicitudin diam. Nam vel ante lorem. Vivamus aliquet purus in magna semper, quis tempor orci volutpat. Cras neque diam, lobortis ac sem vitae, pharetra ultricies nisi. Praesent ut ullamcorper erat.')
-
     ]
 )
